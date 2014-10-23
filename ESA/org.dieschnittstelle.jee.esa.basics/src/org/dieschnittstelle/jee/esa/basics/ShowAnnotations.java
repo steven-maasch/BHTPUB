1a313a96aeb0318c20c0053c6b40e05e68874b24
Steven Maasch <steven.maasch@adesso.de>
2014-10-23 17:19:39 +0200

    Complete UE BAS2

--- ESA/org.dieschnittstelle.jee.esa.basics/src/org/dieschnittstelle/jee/esa/basics/ShowAnnotations.java
+++ ESA/org.dieschnittstelle.jee.esa.basics/src/org/dieschnittstelle/jee/esa/basics/ShowAnnotations.java
@@ -1,11 +1,21 @@
 package org.dieschnittstelle.jee.esa.basics;
 
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.dieschnittstelle.jee.esa.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.jee.esa.basics.annotations.StockItemProxyImpl;
 
 public class ShowAnnotations {
 
+	// the logger
+	protected static Logger logger = Logger
+			.getLogger(ShowAnnotations.class);
+	
 	public static void main(String[] args) {
 		// we initialise the collection
 		StockItemCollection collection = new StockItemCollection(
@@ -28,7 +38,51 @@ public class ShowAnnotations {
 	 * UE BAS2 
 	 */
 	private static void showAttributes(Object consumable) {
+		final String classSimpleName = consumable.getClass().getSimpleName();
+		final Field[] classFields = consumable.getClass().getDeclaredFields();
+		final int classFieldsCount = classFields.length;
+		final int lastFieldIndex = classFieldsCount-1;
+		
+		final StringBuilder sb = new StringBuilder();
+		sb.append("{");
+		sb.append(classSimpleName);
+		sb.append(" ");
+		for (int i = 0; i < classFieldsCount; ++i) {
+			sb.append(classFields[i].getName());
+			sb.append(":");
+			sb.append(invokeGetterForField(classFields[i], consumable));
+			if (i < lastFieldIndex) {
+				sb.append(", ");
+			}
+		}
+		sb.append("}");
+		System.out.println(sb);
+	}
+	
+	private static Object invokeGetterForField(final Field field, final Object object) {
+		try {
+			assert object.getClass().getField(field.getName()) != null;
+			final List<Method> getMethods = getOnlyGetMethods(object.getClass());
+			for (Method method: getMethods) {
+				if (method.getName().toLowerCase().endsWith(field.getName().toLowerCase())) {
+					return method.invoke(object);
+				}
+			}
+			logger.warn("Class doesn't provide getter for field. Returning empty string.");
+			return "";
+		} catch (Exception e) {
+			throw new RuntimeException("Could not invoke getter.", e);
+		}
+	} 
 
+	private static List<Method> getOnlyGetMethods(final Class<?> c) {
+		final List<Method> result = new ArrayList<Method>();
+		for (Method method: c.getMethods()) {
+			if (method.getName().startsWith("get")) {
+				result.add(method);
+			}
+		}
+		return result;
 	}
 
 }
